d10cacacd93240a20d0a0000253eedafd9059eea
Ilua Kisialiou <iluxa91@inbox.ru>
2023-03-28 22:18:08 +0300

    init commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'inst-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['test.component.scss'],
+})
+export class TestComponent {}
