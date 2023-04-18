f4c900cca700b0c1ffc1dbd61b99371125691d2f
Ilua Kisialiou <iluxa91@inbox.ru>
2023-04-18 17:47:53 +0300

    login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -8,23 +8,23 @@ import { AuthService } from '../../../core/services/auth.service'
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  // loginForm = new FormGroup({
-  //   email: new FormControl<string>('', {
-  //     nonNullable: true,
-  //     validators: [
-  //       Validators.required,
-  //       Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}$'),
-  //     ],
-  //   }),
-  //   password: new FormControl<string>('', {
-  //     nonNullable: true,
-  //     validators: [Validators.required, Validators.minLength(3)],
-  //   }),
-  //   rememberMe: new FormControl<boolean>(false, {
-  //     nonNullable: true,
-  //     validators: [Validators.required, Validators.minLength(3)],
-  //   }),
-  // })
+  loginForm = new FormGroup({
+    //   email: new FormControl<string>('', {
+    //     nonNullable: true,
+    //     validators: [
+    //       Validators.required,
+    //       Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}$'),
+    //     ],
+    //   }),
+    //   password: new FormControl<string>('', {
+    //     nonNullable: true,
+    //     validators: [Validators.required, Validators.minLength(3)],
+    //   }),
+    //   rememberMe: new FormControl<boolean>(false, {
+    //     nonNullable: true,
+    //     validators: [Validators.required, Validators.minLength(3)],
+    //   }),
+  })
   constructor(private authService: AuthService) {}
 
   onLoginSubmit() {
